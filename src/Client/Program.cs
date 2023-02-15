a10d8f6a2946062ef451022ea89122d110e8d868
Tim Andre Jacobsen <tymmyh@gmail.com>
2023-02-15 07:07:37 +0100

    Get discoverydocument from authority

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using IdentityModel.Client;
+
+
+Console.WriteLine("starting . . .");
+
+// discover endpoints from metadata
+var client = new HttpClient();
+var disco = await client.GetDiscoveryDocumentAsync("http://localhost:5001");
+if (disco.IsError)
+{
+    Console.WriteLine(disco.Error);
+    Console.ReadLine();
+    return;
+}
+
+Console.WriteLine("end");
+
+// wait for user input to exit console
+Console.ReadLine();
