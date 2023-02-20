01c5949301081a63c3c2f4822c72d03913ae72e5
Tim Andre Jacobsen <tymmyh@gmail.com>
2023-02-20 12:15:02 +0100

    Add authentication to webclient

--- src/WebClient/Program.cs
+++ src/WebClient/Program.cs
@@ -1,4 +1,29 @@
+using System.IdentityModel.Tokens.Jwt;
+
+JwtSecurityTokenHandler.DefaultMapInboundClaims = false;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultScheme = "Cookies";
+        options.DefaultChallengeScheme = "oidc";
+    }).AddCookie("Cookies")
+    .AddOpenIdConnect("oidc", options =>
+    {
+        options.Authority = "https://localhost:5001";
+
+        options.ClientId = "webapp";
+        options.ClientSecret = "Secret";
+        options.ResponseType = "code";
+
+        options.Scope.Clear();
+        options.Scope.Add("openid");
+        options.Scope.Add("profile");
+
+        options.SaveTokens = true;
+    });
 
 // Add services to the container.
 builder.Services.AddRazorPages();
