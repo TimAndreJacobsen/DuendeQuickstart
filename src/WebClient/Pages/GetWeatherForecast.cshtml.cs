0c94b036d7df14e82a2862c40702bcb577b2739c
Tim Andre Jacobsen <tymmyh@gmail.com>
2023-02-22 07:10:54 +0100

    Adds page for protected WeatherForecast Api. Using access token

---
+++ src/WebClient/Pages/GetWeatherForecast.cshtml.cs
@@ -0,0 +1,24 @@
+using System.Net.Http.Headers;
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Authentication;
+
+namespace WebClient.Pages;
+
+public class GetWeatherForecast : PageModel
+{
+    public string Json = string.Empty;
+
+    public async Task OnGet()
+    {
+        var accessToken = await HttpContext.GetTokenAsync("access_token");
+        var client = new HttpClient();
+        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+        
+        var content = await client.GetStringAsync("https://localhost:6001/WeatherForecast");
+        var parsed = JsonDocument.Parse(content);
+        var formatted = JsonSerializer.Serialize(parsed, new JsonSerializerOptions { WriteIndented = true });
+        Json = formatted;
+    }
+}
