923daeb111481839ebf17fcf313b1f38f976837e
Tim Andre Jacobsen <tymmyh@gmail.com>
2023-02-23 15:23:12 +0100

    removes inmemory store and set up EF with SQLite in HostingExtensions.cs

--- src/AuthorityServer/HostingExtensions.cs
+++ src/AuthorityServer/HostingExtensions.cs
@@ -1,3 +1,4 @@
+using Microsoft.EntityFrameworkCore;
 using Serilog;
 
 namespace AuthorityServer;
@@ -6,6 +7,9 @@ internal static class HostingExtensions
 {
     public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
     {
+        var migrationsAssembly = typeof(Program).Assembly.GetName().Name;
+        const string connectionString = @"Data Source=Duende.IdentityServer.Quickstart.EntityFramework.db";
+        
         builder.Services.AddRazorPages();
 
         builder.Services.AddIdentityServer(options =>
@@ -13,9 +17,19 @@ internal static class HostingExtensions
                 // https://docs.duendesoftware.com/identityserver/v6/fundamentals/resources/api_scopes#authorization-based-on-scopes
                 options.EmitStaticAudienceClaim = true;
             })
-            .AddInMemoryIdentityResources(Config.IdentityResources)
-            .AddInMemoryApiScopes(Config.ApiScopes)
-            .AddInMemoryClients(Config.Clients)
+            // .AddInMemoryIdentityResources(Config.IdentityResources)
+            // .AddInMemoryApiScopes(Config.ApiScopes)
+            // .AddInMemoryClients(Config.Clients)
+            .AddConfigurationStore(options =>
+            {
+                options.ConfigureDbContext = b => b.UseSqlite(connectionString,
+                    sql => sql.MigrationsAssembly(migrationsAssembly));
+            })
+            .AddOperationalStore(options =>
+            {
+                options.ConfigureDbContext = b => b.UseSqlite(connectionString,
+                    sql => sql.MigrationsAssembly(migrationsAssembly));
+            })
             .AddTestUsers(TestUsers.Users);
         
         return builder.Build();
